152bfc4618362686684aeecea422a1deca198328
Nurlan270 <nurlanismzade@gmail.com>
2026-09-05 13:16:58 +0400

    chore: add max validation rule for password field

--- internal/core/transport/http/dto/request.go
+++ internal/core/transport/http/dto/request.go
@@ -2,10 +2,10 @@ package dto
 
 type RegisterUserRequest struct {
 	Username string `json:"username" validate:"required,min=3,max=30,username"`
-	Password string `json:"password" validate:"required,min=6"`
+	Password string `json:"password" validate:"required,min=6,max=70"`
 }
 
 type LoginUserRequest struct {
 	Username string `json:"username" validate:"required,min=3,max=30,username"`
-	Password string `json:"password" validate:"required,min=6"`
+	Password string `json:"password" validate:"required,min=6,max=70"`
 }
